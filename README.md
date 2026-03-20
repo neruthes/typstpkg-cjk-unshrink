5189c9aa4507cc8b34d6ccc83566ca49cde83a5c
Neruthes <i@neruthes.xyz>
2026-03-20 13:43:05 +0000

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # typstpkg-cjk-unshrink
+
+
+Copyright (c) 2026 Neruthes. Published using the MIT license.
---
+++ make.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function _die() {
+    echo "$2" > /dev/stderr
+    exit "$1"
+}
+
+
+command -v tomlq || _die 1 "ERROR: Make target 'install_local' requires tomlq"
+command -v rsync || _die 1 "ERROR: Make target 'install_local' requires rsync"
+
+export VER="$(tomlq -r .package.version src/typst.toml)"
+
+case "$1" in
+    pr | submit)
+        universe_dir_pref=../typst-packages-universe/packages/preview/cjk-unshrink
+        universe_dir=../typst-packages-universe/packages/preview/cjk-unshrink/"$VER"
+        # [[ ! -d "$universe_dir" ]] &&
+        # [[ -d "$universe_dir_pref" ]] &&
+        [[ -d ../typst-packages-universe ]] &&
+        rsync --dry-run -av ./src/ --exclude components "$universe_dir/" &&
+        echo "Seems that we can do this!" &&
+        echo '    ' rsync -av ./src/ --exclude components --mkpath "$universe_dir/"
+
+        ;;
+    install_local | i )
+        rsync -auv --delete --mkpath --exclude components       src/     "$HOME"/.local/share/typst/packages/local/cjk-unshrink/"$VER"
+        ;;
+    fast | f)
+        ./make.sh install_local
+        ;;
+    '' )
+        ./make.sh fast
+        ;;
+    * )
+        die 1 "WARNING: No rule to make target '$1'"
+        ;;
+esac
